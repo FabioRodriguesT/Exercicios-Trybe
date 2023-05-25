df0a8cc8cdf04022defee500a7d88df75c9a2498
Fabio Rodrigues <fabur13@gmail.com>
2023-05-25 10:32:14 -0300

    Adicionando arquivos

---
+++ 01-fundamentos/02-section-html-css/01-day-estrutura-de-paginas/parte-1.sh
@@ -0,0 +1,11 @@
+## Parte I - Criação de arquivos e diretórios
+
+# Exercícios - agora, a prática
+
+# 1- Muitas das empresas mais modernas do mundo estão sempre em busca de pessoas desenvolvedoras, e uma das formas de você chamar a atenção delas é ter seu próprio Portfólio Web, onde você irá manter os projetos que desenvolverá!
+
+# 2- Com este exercício, você será capaz de:
+#    Criar seu Portfólio Web usando todo o aprendizado que você construiu hoje;
+#    Hospedar seu Portfólio Web no GitHub Pages.
+
+Link do Portifolio: https://fabiorodriguest.github.io/trybe-portfolio/
